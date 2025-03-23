20126c1acad0816cd2c4225dc465a524547cf040
Carolina Martes <carolina.martes@gmail.com>
2025-03-23 17:01:01 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/venturevibes/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
